1292b28afbdcfc5c60d4d3da3b04ee480db1fe3e
enzing <enzing0120@gmail.com>
2019-07-28 13:45:19 +0900

    git 동작 테스트 - 첫번째 커밋

---
+++ src/main/java/com/enzing/test/gitprjtest/GitprjtestApplication.java
@@ -0,0 +1,13 @@
+package com.enzing.test.gitprjtest;
+
+        import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitprjtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitprjtestApplication.class, args);
+    }
+
+}
